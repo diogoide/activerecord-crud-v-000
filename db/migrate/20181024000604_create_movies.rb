d0828933e13e5dd6fafac0c68891d7adb7d1c983
Diogo Ide <diogoide@gmail.com>
2018-10-24 00:07:30 +0000

    automatically backed up by learn

--- db/migrate/20181024000604_create_movies.rb
+++ db/migrate/20181024000604_create_movies.rb
@@ -1,9 +1,9 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
     create_table :movies do |t|
-      t.string :title 
+      t.string :title
       t.integer :release_date
-      t.string :lead 
+      t.string :lead
       t.boolean :in_theaters
     end 
   end
